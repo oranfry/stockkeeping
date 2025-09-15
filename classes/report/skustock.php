9f870b9db791eae8d8b4e05af89d528950e2ca2a
Oran Fry <oran@emerg.co>
2025-09-15 16:01:17 +1200

    Sku Stock Report
    Patch invalid colon character in group names by breaking sku stock report down into group hierarchy.

--- classes/report/skustock.php
+++ classes/report/skustock.php
@@ -6,7 +6,7 @@ class skustock extends \jars\Report
 {
     function __construct()
     {
-        $this->classify = fn ($line) => [$line->sku];
+        $this->classify = fn ($line) => [str_replace(':', '/', $line->sku)];
         $this->listen = ['adjustmenttransfer', 'consumptiontransfer', 'disposaltransfer', 'purchasetransfer'];
         $this->sorter = fn ($a, $b) => $a->date <=> $b->date;
     }
