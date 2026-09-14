62439ec0f489f61db70dda7935ebab57dded1008
Oran Fry <oran@emerg.co.nz>
2026-09-14 16:37:38 +1200

    Stock Events
    Break classification into year and month, to avoid having too many groups at the top level.

--- Reports/StockEvents.php
+++ Reports/StockEvents.php
@@ -6,13 +6,15 @@ class StockEvents extends \OranFry\Jars\Core\Report
 {
     function __construct()
     {
-        $this->classify = fn ($line) => [substr($line->date, 0, 7)];
+        $this->classify = fn ($line) => [str_replace('-', '/', substr($line->date, 0, 7))];
+
         $this->listen = [
             'adjustment' => (object) ['children' => ['transfers']],
             'consumption' => (object) ['children' => ['transfers']],
             'disposal' => (object) ['children' => ['transfers']],
             'purchase' => (object) ['children' => ['transfers']],
         ];
+
         $this->sorter = fn ($a, $b) => $a->date <=> $b->date;
     }
 }
